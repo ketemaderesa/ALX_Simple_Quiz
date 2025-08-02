aff3c5bbcc9901f3866391b9793cb8317e97ed5b
Ketema Deresa <ketemaderesa123@gmail.com>
2025-08-02 11:03:36 +0100

    Add event listeners for buttons

--- calculator.js
+++ calculator.js
@@ -19,6 +19,27 @@ function getNumbers() {
     const number2 = parseFloat(document.getElementById('number2').value) || 0;
     return { number1, number2 };
 }
+
 function displayResult(result) {
     document.getElementById('calculation-result').textContent = result;
 }
+
+document.getElementById('add').addEventListener('click', () => {
+    const { number1, number2 } = getNumbers();
+    displayResult(add(number1, number2));
+});
+
+document.getElementById('subtract').addEventListener('click', () => {
+    const { number1, number2 } = getNumbers();
+    displayResult(subtract(number1, number2));
+});
+
+document.getElementById('multiply').addEventListener('click', () => {
+    const { number1, number2 } = getNumbers();
+    displayResult(multiply(number1, number2));
+});
+
+document.getElementById('divide').addEventListener('click', () => {
+    const { number1, number2 } = getNumbers();
+    displayResult(divide(number1, number2));
+});
